cef8520ddb3d894af03474e439d9fd647a42880d
Emma Persson <emma.e.persson@me.com>
2015-03-05 08:56:16 +0100

    Insert och remove fungerar alltid!!! :D

--- src/alda/skiplist/SkipList.java
+++ src/alda/skiplist/SkipList.java
@@ -3,13 +3,11 @@ package alda.skiplist;
 public class SkipList<T extends Comparable<? super T>> {
 
 	private int maxLevel;
-	Node<T>[] head;
+	Node<T> head;
 
 	public SkipList(int maxLevel){
 		this.maxLevel = maxLevel;
-		head = new Node[maxLevel];
-		for(int i = 0; i < maxLevel; i++)
-			head[i] = null;
+		head = new Node<T>(maxLevel, null);	//Startnod
 	}
 
 	private class Node<T>{
@@ -21,7 +19,6 @@ public class SkipList<T extends Comparable<? super T>> {
 			this.data = data;
 			this.level = level;
 			next = new Node[level];
-
 			for(int i = 0; i < level; i++){
 				next[i] = null;
 			}
@@ -29,95 +26,58 @@ public class SkipList<T extends Comparable<? super T>> {
 	}
 
 	public void insert(T data){
-		Node<T>[] current = new Node[maxLevel];
-		Node<T>[] previous = new Node[maxLevel];
+		Node<T> current= head;
+		int newLevel=generateLevel();
+		Node<T> newNode= new Node<T>(newLevel, data);
 
-		current[maxLevel - 1] = head[maxLevel - 1];
-		previous[maxLevel - 1] = null;
+		if(head.next[0]==null){ //Om tom skipList, lägg till ny först
+			for(int i = 0; i<=newLevel-1; i++){
+				head.next[i]=newNode;
+			}	
+		}else{ //Annars, hitta vart den ska vara
+			for(int i =newLevel-1; i>=0; i--){
+				while(current.next[i]!=null && current.next[i].data.compareTo(data)<0){
+					current=current.next[i];
 
-		for(int i = maxLevel - 1; i >= 0; i--){
-			while(current[i] != null && current[i].data.compareTo(data) < 0){
-				previous[i] = current[i];
-				current[i] = current[i].next[i];
+				}	
+				newNode.next[i]=current.next[i];//Stoppar in den nya noden.
+				current.next[i]=newNode;
 			}
 
-			if(i > 0){
-				if(previous[i] == null){
-					current[i - 1] = head[i - 1];
-					previous[i - 1] = null;
-				}else{
-					current[i - 1] = previous[i].next[i - 1];
-					previous[i - 1] = previous[i]; 
-				}
-			}
-		}
-
-		int newLevel = generateLevel();
-		System.out.print(newLevel+" ");
-		Node<T> newNode = new Node<T>(newLevel, data);
-
-		for(int i = 0; i <= newLevel - 1; i++){
-			newNode.next[i] = current[i];
-			if(previous[i] == null){
-				head[i] = newNode;
-			}else{
-				previous[i].next[i] = newNode;
-			}
 		}
 	}
-	
-	public boolean remove(T data){
-		
-		Node<T>[] current = new Node[maxLevel];
-		Node<T>[] previous = new Node[maxLevel];
 
-		current[maxLevel - 1] = head[maxLevel - 1];
-		previous[maxLevel - 1] = null;
-
-		for(int i = maxLevel - 1; i >= 0; i--){
-			while(current[i] != null && current[i].data.compareTo(data) < 0 ){
-
-				previous[i] = current[i];
-				current[i] = current[i].next[i];
-			}
-
-			if(i > 0){
-				if(previous[i] == null){
-					current[i - 1] = head[i - 1];
-					previous[i - 1] = null;
+	public void remove(T data){
+		Node<T> current= head;
+		for(int i =maxLevel-1; i>=0; i--){
+			while(current.next[i]!=null && current.next[i].data.compareTo(data)<=0){
+				if(current.next[i].data.equals(data)){ //Om datan är hittad, riktas pekarna om.
+					current.next[i]=current.next[i].next[i];
 				}else{
-					current[i - 1] = previous[i].next[i - 1];
-					previous[i - 1] = previous[i]; 
+					current=current.next[i];//om inte, hoppa till nästa 
 				}
-			}
+			}		
 		}
-		
-			for(int i = 0; i <= current.length-1; i++){
-
-				if(previous[i]!=null){	
-					if(previous[i].next[i] != null && current[i].data.equals(data)){
-						previous[i].next[i] = current[i].next[i];
-					}
-				}
-			}
-		
-
-		
-		
-		return false;
-		
 	}
+
 
 	public T remove(int index){
 		return null;
 	}
 
+	private int generateLevel(){
+		int level = 1;
+		while((int)(Math.random()*2) == 1 && level < maxLevel){
+			level++;
+		}
+		return level;
+	}
 
 	public T get(int index){
 		if(index < 0)
 			return null;
 
-		Node<T> node = head[0];
+		Node<T> node = head.next[0];
 
 		for(int i = 0; i < index; i++){
 			if(node.next[0] != null){
@@ -128,31 +88,7 @@ public class SkipList<T extends Comparable<? super T>> {
 		return node.data;
 	}
 
-	private int generateLevel(){
-		int level = 1;
-
-		while((int)(Math.random()*2) == 1 && level < maxLevel){
-			level++;
-		}
-		return level;
-	}
-
-	public String toString(){
-		Node<T> n = head[0];
-		String result = "SkipList: [";
-
-		while(n != null){
-			result += "'" + n.data.toString() + (n.next[0] != null ? "', " : "'");
-			n = n.next[0];
-		}
-
-		result += "]";
-
-		return result;
-	}
-
 	public static void main(String [] args){
-
 		SkipList<String> mylist = new SkipList<String>(4);
 
 		mylist.insert("hello");
@@ -161,24 +97,24 @@ public class SkipList<T extends Comparable<? super T>> {
 		mylist.insert("you");
 		mylist.insert("doing");
 		mylist.insert("today?");
+		System.out.println("Fullständig lista:");
+		System.out.print(mylist.get(0)+" ");
+		System.out.print(mylist.get(1)+" ");
+		System.out.print(mylist.get(2)+" ");
+		System.out.print(mylist.get(3)+" ");
+		System.out.print(mylist.get(4)+" ");
+		System.out.println(mylist.get(5)+" ");
 
-		System.out.println(" ");
-		System.out.println(mylist);
-		//System.out.println(mylist.get(-1));
 		mylist.remove("you");
-
-		System.out.println(mylist);
-		
 		mylist.remove("hello");
-		System.out.println(mylist);
-		
-		mylist.remove("how");
-		System.out.println(mylist);
-		
-		mylist.remove("today?");
-		System.out.println(mylist);
-		
 		mylist.remove("hi");
-		System.out.println(mylist);
+		System.out.println("Efter remove");
+		System.out.print(mylist.get(0)+" ");
+		System.out.print(mylist.get(1)+" ");
+		System.out.print(mylist.get(2)+" ");
+		System.out.print(mylist.get(3)+" ");
+		System.out.print(mylist.get(4)+" ");
+		System.out.print(mylist.get(5)+" ");
+
 	}
 }
